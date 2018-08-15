2c260a5bd0b06f8bb7200d200b603d137310710b
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-15 14:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 function Objects(){
   var recipes = {}
-  return recipes
+  return typeof recipes === 'object'
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value})
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
-}
-
-function deleteFromObjectByKey(object, key){
-  const newObject = Object.assign({}, object) //"true" copie
-  delete newObject[key]
-  return newObject
-}
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
-  return object
-}
